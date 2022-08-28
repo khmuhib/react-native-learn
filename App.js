7728009899753a59b60510db7db694e01eb252d4
kh muhib <khmuhib2013@gmail.com>
2022-08-28 17:35:13 +0600

    input_text_keyboard_input

--- App.js
+++ App.js
@@ -1,55 +1,36 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 
 export default function App() {
-  const [counter, setCounter] = useState(0)
 
-  const increment = () => {
-    setCounter(counter+1)
-  }
-  const decrement = () => {
-    setCounter(counter-1)
-  }
+  const [text, setText] = useState('sadsa')
 
   return (
-    <View style={style.container}>
-      <TouchableOpacity onPress={increment}>
-        <View style={style.customButton}>
-          <Text style={style.buttonText}>+</Text>
-        </View>
-      </TouchableOpacity>
+    <>
+      <View>
+        <Text>App dsadas</Text>
+        <TextInput style={style.inputBox} placeholder="Name" onChangeText={ value =>{
+          console.log(value)
+          setText(value)
+        }}/>
+        <TextInput style={style.inputBox} placeholder="Age"/>
+        <TextInput style={style.inputBox} placeholder="Phone"/>
+      </View>
+      <View>
+        <Text>{text}</Text>
+      </View>
+    </>
 
-      <Text style={style.counterText}>{counter}</Text>
-      <TouchableOpacity onPress={decrement}>
-        <View style={style.customButton}>
-          <Text style={style.buttonText}>-</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
   )
 }
 
 const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightblue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
-  },
-  customButton: {
-    backgroundColor: 'blue',
-    height: 50,
-    width: 50,
+  inputBox: {
+    borderColor: 'red',
+    borderWidth: 2,
+    margin: 10,
+    paddingStart: 20,
     borderRadius: 50,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 50,
-    textAlign: 'center'
-  },
-  counterText: {
-    fontSize: 32,
+
   }
-}) 
+})
