d31cff1025c56cb42982b28e54fee7e25a92824b
kh muhib <khmuhib2013@gmail.com>
2022-08-28 17:53:25 +0600

    list_view

--- App.js
+++ App.js
@@ -1,36 +1,51 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 
 export default function App() {
 
-  const [text, setText] = useState('sadsa')
-
+  const [list, setlist] = useState([
+    { id: 1, name: 'Kh Muhib' },
+    { id: 2, name: 'Asraf' },
+    { id: 3, name: 'Sifat' },
+    { id: 4, name: 'foysal' },
+    { id: 5, name: 'Shifa' },
+    { id: 6, name: 'Shakib' },
+    { id: 4, name: 'foysal' },
+    { id: 5, name: 'Shifa' },
+    { id: 6, name: 'Shakib' },
+    { id: 4, name: 'foysal' },
+    { id: 5, name: 'Shifa' },
+    { id: 6, name: 'Shakib' },
+  ])
   return (
-    <>
-      <View>
-        <Text>App dsadas</Text>
-        <TextInput style={style.inputBox} placeholder="Name" onChangeText={ value =>{
-          console.log(value)
-          setText(value)
-        }}/>
-        <TextInput style={style.inputBox} placeholder="Age"/>
-        <TextInput style={style.inputBox} placeholder="Phone"/>
-      </View>
-      <View>
-        <Text>{text}</Text>
-      </View>
-    </>
+    <View style={style.container}>
+      <ScrollView>
+        
+      {list.map((listData, index) => {
+        return (
+          <View key={listData.index}>
+            <Text style={style.customText}>{listData.id}: {listData.name}</Text>
+          </View>
+        )
 
+      })}
+      </ScrollView>
+    </View>
   )
 }
 
 const style = StyleSheet.create({
-  inputBox: {
-    borderColor: 'red',
-    borderWidth: 2,
+  container: {
+    backgroundColor: 'red',
     margin: 10,
-    paddingStart: 20,
-    borderRadius: 50,
-
+    flex: 1,
+  },
+  customText: {
+    fontSize: 30,
+    backgroundColor: 'pink',
+    marginTop: 20,
+    marginHorizontal: 20,
+    padding: 20,
+    textAlign: 'center',
   }
 })
