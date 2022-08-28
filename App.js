2cd986c5ab4f6f34d8b5306faeaf5b4a3b91c115
kh muhib <khmuhib2013@gmail.com>
2022-08-28 16:16:27 +0600

    state_counter

--- App.js
+++ App.js
@@ -1,18 +1,55 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
-import { customStyle } from './style'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <View style={customStyle.container}>
-          <View style={customStyle.square}></View>
-        </View>
-      </>
+export default function App() {
+  const [counter, setCounter] = useState(0)
 
-    )
+  const increment = () => {
+    setCounter(counter+1)
   }
+  const decrement = () => {
+    setCounter(counter-1)
+  }
+
+  return (
+    <View style={style.container}>
+      <TouchableOpacity onPress={increment}>
+        <View style={style.customButton}>
+          <Text style={style.buttonText}>+</Text>
+        </View>
+      </TouchableOpacity>
+
+      <Text style={style.counterText}>{counter}</Text>
+      <TouchableOpacity onPress={decrement}>
+        <View style={style.customButton}>
+          <Text style={style.buttonText}>-</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  customButton: {
+    backgroundColor: 'blue',
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 50,
+    textAlign: 'center'
+  },
+  counterText: {
+    fontSize: 32,
+  }
+}) 
